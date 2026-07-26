2e80afa75fbbe15bc70a7df75fcf8aaa3fa60d0a
rajaprabhu-genai <rajaprabhu.genai@gmail.com>
2026-07-26 06:41:36 +0530

    Updated based on code review comments

--- 01_python_basics/employee.py
+++ 01_python_basics/employee.py
@@ -1,28 +1,39 @@
+LINE = "-" * 50
 def calculate_annual_salary(monthly_salary : float) -> float:
     return monthly_salary * 12
 
 def display_employee_details(name : str, emp_id : str, department : str, monthly_salary : float):
     annual_salary = calculate_annual_salary(monthly_salary)
-    employee_category = "Senior Employee" if annual_salary > 50000 else "Junior Employee"
-    print("-" * 50)
+    employee_category = "Senior Employee" if monthly_salary > 50000 else "Junior Employee"
+    print(LINE)
     print("{:^50}".format("Employee Details"))
-    print("-" * 50)
+    print(LINE)
     print(f"ID             : {emp_id}")
     print(f"Name           : {name}")
     print(f"Department     : {department}")
     print(f"Monthly Salary : ${monthly_salary:.2f}")
     print(f"Annual Salary  : ${annual_salary:.2f}")
     print(f"Category       : {employee_category}")
-    print("-" * 50)
+    print(LINE)
 
-def get_employee_details():
-    name = input("Enter employee name: ")
-    emp_id = input("Enter employee ID: ")
-    department = input("Enter employee department: ")
-    monthly_salary = float(input("Enter employee monthly salary: "))
-    return name, emp_id, department, monthly_salary
+def get_employee_details() -> tuple[str, str, str, float]:
+    try:
+        employee_name = input("Enter employee name: ")
+        employee_id = input("Enter employee ID: ")
+        employee_department = input("Enter employee department: ")
+        employee_monthly_salary = float(input("Enter employee monthly salary: "))
+        return employee_name, employee_id, employee_department, employee_monthly_salary
+    except ValueError:
+        print("Invalid input for monthly salary. Please enter a numeric value.")
+        return get_employee_details()      
 
-print("Employee Management System")
+def main(): 
+    print("Employee Management System")
+    add_employee : str = "Y"
+    while add_employee == "Y":
+        employee_name, employee_id, employee_department, employee_monthly_salary = get_employee_details()
+        display_employee_details(employee_name, employee_id, employee_department, employee_monthly_salary)
+        add_employee = input("\nDo you want to add another employee? (Y/N): ").strip().upper()
 
-employee_name, employee_id, employee_department, employee_monthly_salary = get_employee_details()
-display_employee_details(employee_name, employee_id, employee_department, employee_monthly_salary)
+if __name__ == "__main__":
+    main()
